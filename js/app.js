54ad35eb83e3f7c057a763e5b481c17ae19f9322
Juan Ponce <poncejuanmaria@gmail.com>
2021-02-10 10:49:30 -0300

    se agrego validacion Nombre, Consulta e Email

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-function campoRequerido(){
-    console.log('perdi el foco y estoy en la funcion campoRequerido')
-    let input = document.getElementById('nombre');
-    if(input.value === ''){
-        input.className = 'form-control is-invalid';
-    }
-    else{
-        input.className = 'form-control is-valid';
-    }
+function campoRequerido(input) {
+  console.log("perdi el foco y estoy en la funcion campoRequerido");
+  //let input = document.getElementById('nombre');
+  //if (input.value.length >0)
+  if (input.value.trim() === "") {
+    input.className = "form-control is-invalid";
+  } else {
+    input.className = "form-control is-valid";
+  }
+}
+
+//tachiponce@gmail.com EXPRESIONES REGULARES RegExp
+// !='' (es distinto de una cadena vacia) Es para saber si escribió algo o no.
+function validarEmail(email) {
+  let expresion = /\w+@\w+\.[a-z]{2,}$/;
+  if (email.value.trim() != "" && expresion.test(email.value)) {
+    email.className = "form-control is-valid";
+  } else {
+    email.className = "form-control is-invalid";
+  }
 }
